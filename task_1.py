ca6a83fb8432929758e0c553411c1b76cfd8da63
Olamilekan <akintolaolamilekan51@gmail.com>
2023-09-08 00:45:23 +0100

    stage_one

--- task_1.py
+++ task_1.py
@@ -1,35 +1,31 @@
 from flask import *
-import json
 from datetime import datetime, timedelta
 
-app = Flask("Olamilekan")
+app = Flask(__name__)
 
 # validate the current UTC time(with validation of +/-2)
 current_UTC_time = datetime.utcnow()
-
-reserved_UTC_time = datetime.utcnow()
-
 seconds = 2
 
 lower = current_UTC_time - timedelta(seconds=2)
-higher = reserved_UTC_time + timedelta(seconds=2)
+higher = current_UTC_time + timedelta(seconds=2)
 
 if lower <= current_UTC_time and current_UTC_time <= higher:
-    pass
+    status_code = 200
 else:
-    exit(98)
+    status_code = 400
 
-time = current_UTC_time.isoformat()[:-7]
+time = current_UTC_time.strftime("%Y-%m-%dT%H:%M:%SZ")
 day = current_UTC_time.strftime("%A")
 
 data_api = {
-        "slack_name": "example_name",
+        "slack_name": "",
         "current_day": day,
         "utc_time": time,
-        "track": "backend",
+        "track": "",
         "github_file_url": "https://github.com/horlami228/HNGx_track_backend/blob/master/task_1.py",
         "github_repo_url": "https://github.com/horlami228/HNGx_track_backend",
-        "status_code": 200
+        "status_code": status_code
     }
 
 
@@ -42,8 +38,8 @@ def home_page():
 @app.route("/api/", methods=["GET"])
 def api():
     # get request query
-    slack_name = request.args.get("slack_name")
-    track = request.args.get("track")
+    slack_name = request.args.get("slack_name", '')
+    track = request.args.get("track", '')
 
     if slack_name:
         data_api["slack_name"] = slack_name
