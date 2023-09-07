26099449b9acb0b2fb75b760dffbb6e4e16a13e6
Olamilekan <akintolaolamilekan51@gmail.com>
2023-09-07 14:40:33 +0100

    stage_one

--- task_1.py
+++ task_1.py
@@ -13,7 +13,7 @@ reserved_UTC_time = datetime.utcnow()
 seconds = 2
 
 lower = current_UTC_time - timedelta(seconds=2)
-higher = current_UTC_time + timedelta(seconds=2)
+higher = reserved_UTC_time + timedelta(seconds=2)
 
 if lower <= current_UTC_time and current_UTC_time <= higher:
     print("Time is validated")
@@ -27,22 +27,23 @@ def home_page():
 
     query_string = ["slack_name", "track"]
     data_api = {
-        "slack_name": "",
+        "slack_name": " ",
         "current_day": current_UTC_time.strftime("%A"),
         "utc_time": current_UTC_time.strftime("%Y-%m-%dT%H:%M:%SZ"),
-        "track": "",
-        "github_file_url": "https://github.com/horlami228/\
-            HNGx_track_backend/task_1.py",
-        "github_repo_url": "https://github.com/horlami228/HGNx_track_backend",
+        "track": " ",
+        "github_file_url": "https://github.com/horlami228/HNGx_track_backend\
+            /blob/master/task_1.py",
+        "github_repo_url": "https://github.com/horlami228/HNGx_track_backend",
         "status_code": 200
     }
 
     for param_name in query_string:
         param_value = request.args.get(param_name)
-        data_api[param_name] = param_value
+        if param_value is not None:
+            data_api[param_name] = param_value
     json_file = json.dumps(data_api)
     return json_file
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(debug=True)
